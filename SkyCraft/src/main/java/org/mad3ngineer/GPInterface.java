89818ab31c267836f75b3d1578e9e5cdcb22aa2f
Doc <Doc@PCDOC>
2014-04-28 14:11:36 -0600

    Grief Protection interface almost fixed

--- SkyCraft/src/main/java/org/mad3ngineer/GPInterface.java
+++ SkyCraft/src/main/java/org/mad3ngineer/GPInterface.java
@@ -1,6 +1,7 @@
 package org.mad3ngineer;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.CreateClaimResult;
@@ -10,13 +11,21 @@ public class GPInterface {
 
 	public static void protectIsland(Island island){
 		
-		String[] trusted = {island.owner};
 		String[] sk = {"skycraft"};
+		String[] blank = {""};
+		
 		try{
 		GriefPrevention.instance.dataStore.deleteClaim(GriefPrevention.instance.dataStore.getClaimAt(new Location(SkyCraft.getWorld(), island.getCenter().getX(), island.getCenter().getY(), island.getCenter().getZ()), false));
-		}catch(Exception e){}
-		GriefPrevention.instance.dataStore.addClaim(new Claim(new Location(SkyCraft.getWorld(), island.getLowCorner().x, 0, island.getLowCorner().y), new Location(SkyCraft.getWorld(), island.getHighCorner().x, SkyCraft.getWorld().getMaxHeight(), island.getHighCorner().y), "skycraft", trusted, trusted, trusted, sk, (long) ((((int) island.x) << 8) + island.y), false));
+		}catch(Exception e){
+			SkyCraft.getInstance().getLogger().info("No claim existed");
+		}
+		
+		Claim claim = new Claim(new Location(SkyCraft.getWorld(), island.getLowCorner().x, 0, island.getLowCorner().y), new Location(SkyCraft.getWorld(), island.getHighCorner().x, SkyCraft.getWorld().getMaxHeight(), island.getHighCorner().y), "skycraft", blank, blank, blank, sk, (long) ((((int) island.x) << 8) + island.y), true);
+		GriefPrevention.instance.dataStore.addClaim(claim);
+		GriefPrevention.instance.dataStore.saveClaim(claim);
+		
 		addPlayer(island, island.owner);
+		
 		for(String player : island.members){
 			addPlayer(island, player);
 		}
@@ -25,8 +34,21 @@ public class GPInterface {
 	
 	public static void addPlayer(Island island, String player){
 		
-		Claim claim = GriefPrevention.instance.dataStore.getClaimAt(new Location(SkyCraft.getWorld(), island.getCenter().getX(), island.getCenter().getY(), island.getCenter().getZ()), false);
-		claim.allowBuild(SkyCraft.getInstance().getServer().getPlayer(player));
+		Claim claim = GriefPrevention.instance.dataStore.getClaimAt(new Location(SkyCraft.getWorld(), island.getCenter().getX(), island.getCenter().getY(), island.getCenter().getZ()), true);
+
+		if(claim==null){
+			SkyCraft.getInstance().getLogger().info("claim was null");
+			return;
+		}
+		
+		Player p = SkyCraft.getInstance().getServer().getPlayer(player);
+		
+		SkyCraft.getInstance().getLogger().info(p.getName());
+		SkyCraft.getInstance().getLogger().info(p.toString());
+		SkyCraft.getInstance().getLogger().info(String.valueOf(p.hasPermission("test")));
+		
+		claim.allowBuild(p);
+		
 		GriefPrevention.instance.dataStore.saveClaim(claim);
 		
 	}
